a9adf08a20d50614af3068885f79122cdfe4997f
alexandrumanta13 <alexandru.manta@hotmail.com>
2020-08-05 11:02:29 +0300

    chore: homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
-import { UtilsComponent } from './pages/utils/utils.component';
-import { PricesComponent } from './pages/prices/prices.component';
 import { ContactComponent } from './pages/contact/contact.component';
-
-
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ThankYouComponent } from "./components/thank-you/thank-you.component";
-import { OfficesCleaningComponent } from './pages/services/offices-cleaning/offices-cleaning.component';
-import { HomeCleaningComponent } from './pages/services/home-cleaning/home-cleaning.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { ComercialCleaningComponent } from './pages/services/comercial-cleaning/comercial-cleaning.component';
-import { IndustrialCleaningComponent } from './pages/services/industrial-cleaning/industrial-cleaning.component';
-import { ConstructorCleaningComponent } from './pages/services/constructor-cleaning/constructor-cleaning.component';
-import { EventsCleaningComponent } from './pages/services/events-cleaning/events-cleaning.component';
-import { AutoCleaningComponent } from './pages/services/auto-cleaning/auto-cleaning.component';
-
-import { CouchCleaningComponent } from './pages/cleaning/couch-cleaning/couch-cleaning.component';
-import { FloorCleaningComponent } from './pages/cleaning/floor-cleaning/floor-cleaning.component';
-import { CarpetCleaningComponent } from './pages/cleaning/carpet-cleaning/carpet-cleaning.component';
-
 import { GdprComponent } from './pages/gdpr/gdpr.component';
 import { ConfidentialityComponent } from './pages/confidentiality/confidentiality.component';
 import { CookiesComponent } from "./pages/cookies/cookies.component";
@@ -34,133 +16,6 @@ const routes: Routes = [
     data: {
       title: 'Curatenie perfecta',
       description: 'Description Meta Tag Content',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'despre-noi', component: AboutComponent,
-    data: {
-      title: 'Despre noi',
-      description: 'Apeleaza la servicii profesionale de curatenie la domiciliu. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov. Preturi competitive.Description Meta Tag Content',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'servicii-curatenie', component: ServicesComponent,
-    data: {
-      title: 'Servicii curatenie',
-      description: 'Apeleaza la servicii profesionale de curatenie la domiciliu. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov. Preturi competitive.',
-      ogUrl: 'your og url'
-    }
-  },
-
-
-  {
-    path: 'curatenie-birouri', component: OfficesCleaningComponent,
-    data: {
-      title: 'Curatenie Birouri',
-      description: 'Apeleaza la servicii profesionale de curatenie birouri. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'curatenie-la-domiciliu', component: HomeCleaningComponent,
-    data: {
-      title: 'Curatenie la domiciliu',
-      description: 'Apeleaza la servicii profesionale de curatenie la domiciliu. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov. Preturi competitive.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'curatenie-spatii-comerciale', component: ComercialCleaningComponent,
-    data: {
-      title: 'Curatenie spatii comerciale',
-      description: 'Apeleaza la servicii profesionale de curatenie spatii comerciale. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov. Preturi competitive.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'curatenie-spatii-industriale', component: IndustrialCleaningComponent,
-    data: {
-      title: 'Curatenie spatii industriale',
-      description: 'Apeleaza la servicii profesionale de curatenie industriala. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov. Preturi competitive.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'curatenie-dupa-constructor', component: ConstructorCleaningComponent,
-    data: {
-      title: 'Curatenie dupa constructor',
-      description: 'Apeleaza la servicii profesionale de curatenie dupa constructor sau renovare. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'curatenie-dupa-evenimente', component: EventsCleaningComponent,
-    data: {
-      title: 'Curatenie evenimente',
-      description: 'Apeleaza la servicii profesionale de curatenie inainte si dupa evenimente. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov. Preturi competitive.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'curatare-tapiterie-auto-detailing', component: AutoCleaningComponent,
-    data: {
-      title: 'Curatare tapiterie auto',
-      description: 'Vrei curatare tapiterie auto si detailing auto la cel mai bun raport pret? Suntem o firma de curatenie profesionala din bucuresti si Ilfov.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'curatare-canapele', component: CouchCleaningComponent,
-    data: {
-      title: 'Curatare canapele si fotolii',
-      description: 'Apeleaza la servicii profesionale de curatare canapele la domiciliu. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'spalare-mecanizata-podele', component: FloorCleaningComponent,
-    data: {
-      title: 'Spalare mecanizata podele',
-      description: 'Vrei ca podelele sa-si recapete stralucirea de la inceput? Apeleaza la serviciile de spalare mecanizata podele. Ne deplasam oriunde in Bucuresti si Ilfov.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'curatare-mocheta-covor', component: CarpetCleaningComponent,
-    data: {
-      title: 'Curatare mocheta',
-      description: 'Apeleaza la servicii profesionale de curatare mocheta si covor la domiciliu. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov. Preturi competitive.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'utile', component: UtilsComponent,
-    data: {
-      title: 'Utile',
-      description: 'Apeleaza la servicii profesionale de curatare mocheta si covor la domiciliu. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov. Preturi competitive.',
-      ogUrl: 'your og url'
-    }
-  },
-
-  {
-    path: 'preturi', component: PricesComponent,
-    data: {
-      title: 'Preturi',
-      description: 'Apeleaza la servicii profesionale de curatare mocheta si covor la domiciliu. Suntem o firma de curatenie ce oferim servicii in Bucuresti si Ilfov. Preturi competitive.',
       ogUrl: 'your og url'
     }
   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { NgImageSliderModule } from 'ng-image-slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
-import { SliderSectionComponent } from './pages/home/slider-section/slider-section.component';
-import { ServiceSectionComponent } from './pages/home/service-section/service-section.component';
-import { AboutComponent } from './pages/about/about.component';
+
+
 import { QuickFormContactComponent } from './components/quick-form-contact/quick-form-contact.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HomeComponent } from './pages/home/home.component';
+
+
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { FormContactCategoriesComponent } from './components/form-contact-categories/form-contact-categories.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { OfficesCleaningComponent } from './pages/services/offices-cleaning/offices-cleaning.component';
-import { MenuServicesComponent } from './pages/services/menu-services/menu-services.component';
-import { LinkServicesComponent } from './pages/services/link-services/link-services.component';
-import { LinksComponent } from './pages/services/link-services/links/links.component';
+
 import { SimpleFromContactComponent } from './components/simple-from-contact/simple-from-contact.component';
-import { HomeCleaningComponent } from './pages/services/home-cleaning/home-cleaning.component';
-import { ComercialCleaningComponent } from './pages/services/comercial-cleaning/comercial-cleaning.component';
-import { IndustrialCleaningComponent } from './pages/services/industrial-cleaning/industrial-cleaning.component';
-import { ConstructorCleaningComponent } from './pages/services/constructor-cleaning/constructor-cleaning.component';
-import { EventsCleaningComponent } from './pages/services/events-cleaning/events-cleaning.component';
-import { AutoCleaningComponent } from './pages/services/auto-cleaning/auto-cleaning.component';
-import { CouchCleaningComponent } from './pages/cleaning/couch-cleaning/couch-cleaning.component';
-import { FloorCleaningComponent } from './pages/cleaning/floor-cleaning/floor-cleaning.component';
-import { CarpetCleaningComponent } from './pages/cleaning/carpet-cleaning/carpet-cleaning.component';
-import { ServiceBiocidComponent } from './pages/home/service-biocid/service-biocid.component';
-import { ServiceTestimonialsComponent } from './pages/home/service-testimonials/service-testimonials.component';
-import { GalleryCarouselComponent } from './components/gallery-carousel/gallery-carousel.component';
-import { ServicePerfectCleanComponent } from './pages/home/service-perfect-clean/service-perfect-clean.component';
-import { AccordionComponent } from './components/accordion/accordion.component';
-import { UtilsComponent } from './pages/utils/utils.component';
+
 import { ContactComponent } from './pages/contact/contact.component';
 import { GdprComponent } from './pages/gdpr/gdpr.component';
 import { ConfidentialityComponent } from './pages/confidentiality/confidentiality.component';
 import { CookiesComponent } from './pages/cookies/cookies.component';
 import { TermsComponent } from './pages/terms/terms.component';
 import { ThankYouComponent } from './components/thank-you/thank-you.component';
-import { PricesComponent } from './pages/prices/prices.component';
 import { WhatsappComponent } from './components/whatsapp/whatsapp.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SliderComponent } from './pages/home/slider/slider.component';
+import { MobileMenuComponent } from './components/layout/mobile-menu/mobile-menu.component';
+import { ProductQuickViewComponent } from './components/layout/product-quick-view/product-quick-view.component';
+
   
 
 @NgModule({
@@ -54,42 +39,22 @@ import { WhatsappComponent } from './components/whatsapp/whatsapp.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SliderSectionComponent,
-    ServiceSectionComponent,
-    AboutComponent,
     QuickFormContactComponent,
     HomeComponent,
     NotFoundComponent,
     FormContactCategoriesComponent,
-    ServicesComponent,
-    OfficesCleaningComponent,
-    MenuServicesComponent,
-    LinkServicesComponent,
-    LinksComponent,
     SimpleFromContactComponent,
-    HomeCleaningComponent,
-    ComercialCleaningComponent,
-    IndustrialCleaningComponent,
-    ConstructorCleaningComponent,
-    EventsCleaningComponent,
-    AutoCleaningComponent,
-    CouchCleaningComponent,
-    FloorCleaningComponent,
-    CarpetCleaningComponent,
-    ServiceBiocidComponent,
-    ServiceTestimonialsComponent,
-    GalleryCarouselComponent,
-    ServicePerfectCleanComponent,
-    AccordionComponent,
-    UtilsComponent,
     ContactComponent,
     GdprComponent,
     ConfidentialityComponent,
     CookiesComponent,
     TermsComponent,
     ThankYouComponent,
-    PricesComponent,
-    WhatsappComponent
+    WhatsappComponent,
+    SliderComponent,
+    MobileMenuComponent,
+    ProductQuickViewComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -98,7 +63,6 @@ import { WhatsappComponent } from './components/whatsapp/whatsapp.component';
     HttpClientModule,
     ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    NgImageSliderModule
     
   ],
   exports: [
